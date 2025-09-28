be9ffe5c56ff41b133afd587a00dba73bcc467e7
Shizune-bots <shizune080@gmail.com>
2025-09-28 16:39:11 +0530

    Update dl_helpers.py

--- bot/workers/downloaders/dl_helpers.py
+++ bot/workers/downloaders/dl_helpers.py
@@ -1,6 +1,6 @@
 import uuid
 
-from bot import asyncio, math, os, pyro, qb_lock, qbClient, time
+from bot import asyncio, math, os, pyro, qb_lock, qbclient, time
 from bot.config import _bot, conf
 from bot.utils.bot_utils import (
     Qbit_c,
